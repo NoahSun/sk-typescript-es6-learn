5e8d3912a00b8b8d423ec14fe1c97adcaa22d060
NoahSun <NoahSun@users.noreply.github.com>
2017-07-20 18:28:14 +0800

    添加示例

--- src/app/decorator/1_base_decorator.ts
+++ src/app/decorator/1_base_decorator.ts
@@ -3,9 +3,10 @@
  * @email 
  * @create date 2017-07-18 06:48:11
  * @modify date 2017-07-18 06:48:11
- * @desc 第一个decorator（装饰器）
+ * @desc 第一个decorator（装饰器），修饰类
 */
 
+// 例一：
 function testable(target) {
     target.isTestable = true;
     return;
@@ -18,3 +19,32 @@ export class MyTestableClass {
 }
 
 console.log(MyTestableClass.isTestable);
+
+// 例二：
+function sealed(target: Function) {
+    Object.seal<Function>(target);
+    Object.seal(target.prototype);
+}
+
+@sealed
+class MyClass {
+    constructor() {
+        console.log(1);
+    }
+    a: number = 2;
+    b: string = 'three';
+    [otherProp: string]: any;
+}
+
+const mc = new MyClass();
+mc.b = '1';
+mc.c = 3;
+try {
+    MyClass.prototype.c = 3
+}
+catch (e) {
+    console.log('使用了sealed装饰器后不能配置(添加、删除)类的属性，只能更改值');
+}
+
+console.log(mc.c);
+console.log(MyClass.prototype.c);
