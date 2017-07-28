d1f35fdf3973ab3648114ff2e631227c8fc9f910
NoahSun <NoahSun@users.noreply.github.com>
2017-07-28 12:36:00 +0800

    类——存取器

---
+++ src/app/ts_class/4_accessors.ts
@@ -0,0 +1,34 @@
+/**
+ * @author Noah Sun
+ * @email 
+ * @create date 2017-07-28 12:10:42
+ * @modify date 2017-07-28 12:10:42
+ * @desc 类——存取器
+ * 注意：
+ *       1.存取器要求ts编译器设置输出目标为ES5或更高。不支持降级到ES3。
+ *       2.只带有get不带有set的存取器自动判断为readonly。
+*/
+
+export let passcode = "secret passcode";
+
+class Employee {
+    private _fullname: string;
+    get fullname(): string {
+        return this._fullname;
+    }
+    set fullname(newName: string) {
+        if (!newName) {
+            throw "Error: newName is empty!";
+        }
+        if (passcode && passcode === "secret passcode") {
+            this._fullname = newName;
+            console.log("object");
+        } else {
+            throw "Error: Unauthorized update of employee!";
+        }
+    }
+}
+
+let employee = new Employee();
+employee.fullname = "";
+
