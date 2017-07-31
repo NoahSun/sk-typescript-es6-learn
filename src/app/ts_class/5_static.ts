e095b4f0d4f020f4a4c0e6d28f0ed4b67fd23a06
NoahSun <NoahSun@users.noreply.github.com>
2017-07-31 10:43:36 +0800

    类——修饰符（static）

---
+++ src/app/ts_class/5_static.ts
@@ -0,0 +1,32 @@
+/**
+ * @author Noah Sun
+ * @email 
+ * @create date 2017-07-28 12:36:17
+ * @modify date 2017-07-28 12:36:17
+ * @desc 类——静态属性
+*/
+
+export class Grid {
+    private static origin = {
+        x: 0,
+        y: 0
+    }
+    constructor() {
+
+    }
+    set origin(point: { x: number, y: number }) {
+        Grid.origin = point;
+    }
+    get origin() {
+        return Grid.origin;
+    }
+}
+
+const grid = new Grid();
+console.log(grid.origin);
+grid.origin = {
+    x: 1, y: 2
+}
+console.log(grid.origin);
+// static也可以加上private修饰符，但是就不能直接用Grid.origin了
+// console.log(Grid.origin);
