e671208b58c77c2c854ea2d01b2bcbfd8c760a34
NoahSun <NoahSun@users.noreply.github.com>
2017-08-09 19:05:18 +0800

    高级类型——联合类型

--- src/app/ts_advance_type/2_union_type.ts
+++ src/app/ts_advance_type/2_union_type.ts
@@ -83,3 +83,73 @@ if (isFish(pet)) {
     // (<Snake>pet).crawl();
     // (<Bird>pet).fly()
 }
+
+// typeof 类型保护
+function isNumber(x: any): x is number {
+    return typeof x === 'number';
+}
+
+function isString(x: any): x is string {
+    return typeof x === 'string';
+}
+
+function padLeft1(value: string, padding: string | number) {
+    if (isNumber(padding)) {
+        return Array(padding + 1).join(" ") + value;
+    }
+    if (isString(padding)) {
+        return padding + value;
+    }
+    throw new Error(`Expected string or number, got '${padding}'.`);
+}
+
+// instanceof 类型保护
+// 是通过构造函数来细化类型的一种方式
+/**
+ * instanceof 的右侧要求是一个构造函数，Typescript将细化为：
+ * 1.此构造函数的 prototype 属性的类型，如果它的类型不为 any 的话
+ * 2.构造签名说返回的联合类型
+ */
+interface Padder {
+    getPaddingString(): string
+}
+
+class SpaceRepeatingPadder implements Padder {
+    constructor(private numSpaces: number) { }
+    getPaddingString() {
+        return Array(this.numSpaces + 1).join(" ");
+    }
+}
+
+class StringPadder implements Padder {
+    constructor(private value: string) { }
+    getPaddingString() {
+        return this.value;
+    }
+}
+
+function getRandomPadder() {
+    return Math.random() < 0.5 ?
+        new SpaceRepeatingPadder(4) :
+        new StringPadder(" ");
+}
+
+let padder: Padder = getRandomPadder();
+if (padder instanceof SpaceRepeatingPadder) {
+    padder; // 类型细化为 SpaceRepeatingPadder
+}
+if (padder instanceof StringPadder) {
+    padder; // 类型细化为 StringPadder
+}
+
+// 如果编译器不能够去除null 或 undefined,你可以使用添加 ! 后缀的方法来手动去除
+function broken(name: string | null): string {
+    function postfix(epithet: string) {
+        // Error! name is possibly 'null'
+        // return name.charAt(0) + '. the ' + epithet;
+        return name!.charAt(0) + '. the ' + epithet;
+        //         ^ 使用 ! 来去除 null 和 undefined
+    }
+    name = name || 'Bob';
+    return postfix('great');
+}
